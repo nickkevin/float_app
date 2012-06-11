b559764f38178ab6fec4b6a572181ac267209c85
nickkevin <nickkevin35@gmail.com>
2012-06-11 17:53:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FloatApp::Application.config.secret_token = 'e532ded66a2b48655160d2a650719e787ab5ab95bdcce2f54e804dd48265bbde04cd5fbfa814d472b33ba7d04eb2edb11b73b21d7f7b026d51a97bd45708f01c'
